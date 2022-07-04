52378696829a5d9406f6ad6a2868a7e93957565d
gouranga satapathy <pulusatapathy@gmail.com>
2022-07-04 21:28:29 +0530

    added sidebar of dashboard page

--- src/main/java/com/example/contactbook/controller/Maincontroller.java
+++ src/main/java/com/example/contactbook/controller/Maincontroller.java
@@ -11,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
