00378ec8f440e25c988b00dfed4a47f30d3852f6
gouranga satapathy <pulusatapathy@gmail.com>
2022-07-05 11:31:14 +0530

    contact page created, fetching the contact details & printing them in console is done successfully.

--- src/main/java/com/example/contactbook/controller/UserController.java
+++ src/main/java/com/example/contactbook/controller/UserController.java
@@ -2,26 +2,60 @@ package com.example.contactbook.controller;
 
 import java.security.Principal;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.example.contactbook.entities.Contact;
 import com.example.contactbook.repo.UserRepo;
 
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements WebMvcConfigurer{
     @Autowired
     private UserRepo userRepo;
 
-    @GetMapping("/index")
-    public String getIndex(Model model,Principal principal) {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/dashboard/dashboard").setViewName("dashboard/dashboard");
+    }
+
+    @ModelAttribute
+    public void defaultRun(Model model, Principal principal) {
         var userDetails = userRepo.getUserByUserName(principal.getName());
         model.addAttribute("user", userDetails);
-        // System.out.println(userDetails);
-        return "dashboard";
+
+    }
+
+    @GetMapping("/index")
+    public String getIndex() {
+        return "dashboard/main";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashBoard(Model model) {
+        model.addAttribute("contact", new Contact());
+        return "dashboard/dashboard";
+    }
+
+    @PostMapping("/addcontact")
+    public String saveContact(Model model, @ModelAttribute("contact") Contact contact, BindingResult bindingResult,
+            @RequestParam(value = "checkbox", defaultValue = "false") boolean checkbox,
+            HttpSession session) {
+
+        System.out.println(contact);
+
+        return "dashboard/dashboard";
     }
 
 }
